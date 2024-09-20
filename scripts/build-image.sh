1047830bb344afea13ef59948f867eb803d0b23d
HarshithaShashidhara <162761807+HarshithaShashidhara@users.noreply.github.com>
2024-09-20 23:32:46 +1000

    Update build-image.sh

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 #
-# Builds a Docker image.
+# Builds Docker images for book-catalog and inventory-management microservices.
 #
 # Environment variables:
 #
@@ -11,9 +13,12 @@
 #       ./scripts/build-image.sh
 #
 
-set -u # or set -o nounset
+set -u  # Fail if undefined variable is used
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
-docker build -t $CONTAINER_REGISTRY/book-catalog:$VERSION --file ./Dockerfile .
-docker build -t $CONTAINER_REGISTRY/inventory-management:$VERSION --file ./Dockerfile .
+# Build Docker image for book-catalog microservice
+docker build -t $CONTAINER_REGISTRY/book-catalog:$VERSION --file ./book_catalog/Dockerfile ./book_catalog
+
+# Build Docker image for inventory-management microservice
+docker build -t $CONTAINER_REGISTRY/inventory-management:$VERSION --file ./inventory_management/Dockerfile ./inventory_management
